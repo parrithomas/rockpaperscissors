786bfff348226a7208f0d9cab21aa7067b9adac2
YeastyBoy <emailparri@gmail.com>
2020-05-19 18:51:13 +0100

    added score function to script.js

--- readme.md
+++
@@ -1,3 +0,0 @@
-<h1>Rock, Paper, Scissors</h1>
-A simple Rock, Paper, Scissors game created with vanilla Javascript.<br>
-<em>An Odin Project assignment.</em>
--- script.js
+++ script.js
@@ -25,55 +25,88 @@ let playerScore = 0; // set the player score
 let computerScore = 0; // set the computer score
 
 function playRound(playerSelection, computerSelection){
-    if (playerSelection === computerSelection){ // check to see if the game is a tie
+    
+    // check to see if the game is a tie
+    if (playerSelection === computerSelection){
         return 'The game is a tie';
     }
 
-    if (playerSelection === 'rock'){ // runs checks if player throws rock
+    if (playerSelection === 'rock'){
         if (computerSelection === 'paper'){
-            computerScore += 1;
+            computerScore +=1;
             return "You lose! Paper beats rock.";
         } else {
-            playerScore += 1;
-            return "You win! Rock beats scissors!"
+            playerScore +=1;
+            return "You win! Rock beats scissors!";
         }
     }
 
     if (playerSelection === 'paper'){
         if (computerSelection === 'scissors'){
-            computerScore += 1;
+            computerScore +=1;
             return 'You lose! Scissors beats paper.';
         } else {
-            playerScore += 1;
-            return 'You win! Paper beats rock!'
+            playerScore +=1;
+            return 'You win! Paper beats rock!';
             }
         }
 
     if(playerSelection === 'scissors'){
         if (computerSelection === 'rock'){
-            computerScore += 1;
+            computerScore +=1;
             return 'You lose! Rock beats scissors.';
         } else {
-            playerScore += 1;
+            playerScore +=1;
             return 'You win! Scissors beats paper!';
         }
     }
 }
 
 let computerSelection = computerPlay(); // assign return of computerPlay() to a variable
-let playerSelection = playerThrow(prompt("Rock, paper or scissors?","")); // takes player's choice and assigns to a variable
+let playerSelection = playerThrow('rock'); // takes player's choice and assigns to a variable 
 
-function game(){
-     
-    playRound(playerSelection, computerSelection);
-    playRound(playerSelection, computerSelection);
-    playRound(playerSelection, computerSelection);
-    playRound(playerSelection, computerSelection);
-}
-
-// print result to html
+// let playerSelection = playerThrow(prompt("Rock, paper or scissors?","")); // takes player's choice and assigns to a variable 
+/*
+// print result
 console.log(`The player chose: ${playerSelection}`);
 console.log(`The computer chose: ${computerSelection}`);
 console.log(playRound(playerSelection, computerSelection));
 console.log(`Player: ${playerScore} // Computer ${computerScore}`)
+*/
+
+function playMatch(){
+    playerSelection = playerThrow(prompt("Rock, paper or scissors?",""));
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    console.log(`The player chose: ${playerSelection} // The computer chose: ${computerSelection}`);
+    console.log(playRound(playerSelection, computerSelection));
+
+    playerSelection = playerThrow(prompt("Rock, paper or scissors?",""));
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    console.log(`The player chose: ${playerSelection} // The computer chose: ${computerSelection}`);
+    console.log(playRound(playerSelection, computerSelection));
+
+    playerSelection = playerThrow(prompt("Rock, paper or scissors?",""));
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    console.log(`The player chose: ${playerSelection} // The computer chose: ${computerSelection}`);
+    console.log(playRound(playerSelection, computerSelection));
+
+    playerSelection = playerThrow(prompt("Rock, paper or scissors?",""));
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    console.log(`The player chose: ${playerSelection} // The computer chose: ${computerSelection}`);
+    console.log(playRound(playerSelection, computerSelection));
+
+    playerSelection = playerThrow(prompt("Rock, paper or scissors?",""));
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    console.log(`The player chose: ${playerSelection} // The computer chose: ${computerSelection}`);
+    console.log(playRound(playerSelection, computerSelection));
+
+    console.log(`Player: ${playerScore} // Computer ${computerScore}`)
+}
+
+playMatch();
 
